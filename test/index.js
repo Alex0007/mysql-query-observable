c8e7a2edacbd5e58e25652c314ead5504135aa82
Alex0007 <Alex0007lolpvp@gmail.com>
2016-08-20 15:18:56 +0500

    upd

--- test/index.js
+++ test/index.js
@@ -2,13 +2,13 @@ let test = require('ava')
 let createObservableFromQuery = require('../')
 
 test.cb('test if basic query working', t => {
-  createObservableFromQuery(`
+  t.plan(2)
+
+  return createObservableFromQuery(`
     SELECT * FROM item ORDER BY id ASC LIMIT 2
   `).toArray()
     .do(data => {
       t.is(data.length, 2, 'Correct number of items receieved')
       t.is(data[0].id, 85435, 'Correct item')
-      t.end()
     })
-    .subscribe()
 })
